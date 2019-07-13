e525de804df055b86c4552b2299649b8c826732d
chrissx@ThinkBadly <chrissx@chrissx.ga>
2019-07-13 23:35:41 +0200

    shortened code, improved readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gamix - a simple scripted setup for all your gaming on linux
 Just run `netsh -C ./gamix.sh -c chrissxYT/gamix`.
+## deps
+This depends on `netsh` and `udpkg`.
--- minecraft.sh
+++ minecraft.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-curl -L -o mc.deb https://launcher.mojang.com/download/Minecraft.deb
-sudo dpkg -i mc.deb
-sudo apt -f install
-rm -f mc.deb
+udpkg https://launcher.mojang.com/download/Minecraft.deb
