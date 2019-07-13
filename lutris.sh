1e9f0f4bd01f28fdd740a6337f5b6bfe64254eee
chrissx@ThinkBadly <chrissx@chrissx.ga>
2019-07-13 23:31:05 +0200

    improved i3buntu compatibility (yea, this wasnt really i3b compat for a long time xd)

--- lutris.sh
+++ lutris.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-ver=$(lsb_release -sr); if [ $ver != "18.10" -a $ver != "18.04" -a $ver != "16.04" ]; then ver=18.04; fi
-echo "deb http://download.opensuse.org/repositories/home:/strycore/xUbuntu_$ver/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list
-wget -q https://download.opensuse.org/repositories/home:/strycore/xUbuntu_$ver/Release.key -O- | sudo apt-key add -
+v=$(lsb_release -sr)
+[ $v = "19" ] && v=19.04
+[ $v = "18" ] && v=18.04
+if [ $v != "19.04" -a $v != "18.10" -a $v != "18.04" -a $v != "16.04" ]
+then
+        v=18.04
+fi
+echo "deb http://download.opensuse.org/repositories/home:/strycore/xUbuntu_$v/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list
+curl https://download.opensuse.org/repositories/home:/strycore/xUbuntu_$v/Release.key | sudo apt-key add -
 sudo apt update
 sudo apt install lutris
